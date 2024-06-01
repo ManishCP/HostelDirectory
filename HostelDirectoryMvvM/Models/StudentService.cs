147f1a9228303798ac4a13596b6a7abb5cd85828
ManishCP <manishcp77@gmail.com>
2024-06-01 05:37:28 -0400

    Creating Services

---
+++ HostelDirectoryMvvM/Models/StudentService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostelDirectoryMvvM.Models
+{
+    internal class StudentService
+    {
+        private static List<Student> ObjStudentsList;
+        public StudentService() 
+        {
+            ObjStudentsList = new List<Student>();
+            {
+                new Student { Id = 101, Name = "Manish", Age = 23, RoomNumber = 508 };
+            }
+        }
+
+        public List<Student> GetAll()
+        { 
+            return ObjStudentsList; 
+        }
+
+        public bool Add(Student objNewStudent) 
+        {
+            if ((objNewStudent.Age > 21 || objNewStudent.Age < 25))
+                throw new ArgumentException("Invalid age limit for the student");
+
+            ObjStudentsList.Add(objNewStudent);
+            return true;
+        }
+
+        public bool Update(Student objNewStudentToUpdate)
+        {
+            bool IsUpdated = false;
+            for (int index = 0; index < ObjStudentsList.Count; index++)
+            {
+                if (ObjStudentsList[index].Id == objNewStudentToUpdate.Id)
+                {
+                    ObjStudentsList[index].Name = objNewStudentToUpdate.Name;
+                    ObjStudentsList[index].Age = objNewStudentToUpdate.Age;
+                    IsUpdated = true;
+                    break;
+                }
+            }
+            return IsUpdated;
+        }
+
+        public bool Delete(int id)
+        {
+            bool IsDeleted = false;
+            for (int index = 0; index < ObjStudentsList.Count; index++)
+            {
+                if (ObjStudentsList[index].Id == id)
+                {
+                    ObjStudentsList.RemoveAt(index);
+                    IsDeleted = true;
+                    break;
+                }
+            }
+            return IsDeleted;
+        }
+
+        public Student Search(int id)
+        { 
+            return ObjStudentsList.FirstOrDefault(e => e.Id == id); 
+        }
+
+    }
+}
