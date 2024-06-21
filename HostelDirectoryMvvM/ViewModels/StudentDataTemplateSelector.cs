ca34cae973741f98f8b64549a99224f38d3e31b7
ManishCP <manishcp77@gmail.com>
2024-06-21 18:24:32 -0400

    Dynamic View - When selecting/deselecting the change of Editor view and Empty View implemented. - Made the editor view binding to StudentViewModel rather than MainViewModel, because it deals with particular student.

---
+++ HostelDirectoryMvvM/ViewModels/StudentDataTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Diagnostics;
+
+namespace HostelDirectoryMvvM.ViewModels
+{
+    public class StudentDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate StudentEditorViewTemplate { get; set; }
+        public DataTemplate EmptyViewTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            // Debugging to understand the selection process
+            if (item != null && item is StudentViewModel studentViewModel && !string.IsNullOrEmpty(studentViewModel.StudentID))
+            {
+                Debug.WriteLine($"Selecting template for {((StudentViewModel)item).GetType().Name}");
+                return StudentEditorViewTemplate;
+            }
+            else
+            {
+                Debug.WriteLine($"Selecting EmptyViewTemplate");
+                return EmptyViewTemplate;
+            }
+        }
+    }
+}
